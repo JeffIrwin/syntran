626bcd8ab937b50af59110a9ae97b0b14fc1e007
Jeff Irwin <jirwin505@gmail.com>
2023-01-02 19:30:07 -0500

    Add string eval() fn and some tests

--- src/main.f90
+++ src/main.f90
@@ -752,7 +752,96 @@ end subroutine interpret
 
 !===============================================================================
 
+integer function eval(str)
+
+	character(len = *), intent(in) :: str
+
+	eval = syntax_eval(syntax_parse(str))
+
+end function eval
+
+!===============================================================================
+
 end module core_m
+
+!===============================================================================
+
+subroutine unit_test_bin_arith(npass, nfail)
+
+	use core_m
+
+	integer, intent(inout) :: npass, nfail
+
+	!********
+
+	character(len = *), parameter :: label = 'binary arithmetic'
+
+	integer :: id(1)
+
+	logical, allocatable :: tests(:)
+
+	write(*,*) 'Unit testing '//label//' ...'
+
+	! TODO: subtraction, division, more in general
+
+	tests = &
+		[   &
+			eval('1') == 1, &
+			eval('69') == 69, &
+			eval('420') == 420, &
+			eval('1 + 2') == 1 + 2, &
+			eval('1 + 2 + 34') == 1 + 2 + 34, &
+			eval('1 + 2 * 3') == 1 + 2 * 3, &
+			eval('1 * 2 * 3 * 4') == 1 * 2 * 3 * 4, &
+			eval('73 - 48') == 73 - 48, &
+			eval('73 - 48 - 21') == 73 - 48 - 21, &
+			eval('24 / 6') == 24 / 6, &
+			eval('24 / 6 / 2') == 24 / 6 / 2, &
+			eval('343 - 87654345 / 27 + 76 * 234 - 65432 / 63') &
+			   == 343 - 87654345 / 27 + 76 * 234 - 65432 / 63 &
+		]
+
+	if (.not. all(tests)) then
+
+		write(*, '(a,i0,a)') '     Error: ', count(.not. tests), &
+				' '//label//' test(s) failed'
+
+		id = findloc(tests, .false.)
+		write(*, '(a,i0,a)') '     Test ID ', id(1), ' was the first failure'
+
+	end if
+
+	npass = npass + count(tests)
+	nfail = nfail + count(.not. tests)
+
+end subroutine unit_test_bin_arith
+
+!===============================================================================
+
+subroutine unit_tests()
+
+	integer :: npass, nfail
+
+	! TODO: move to separate program
+
+	write(*,*) repeat('=', 72)
+	write(*,*) 'Running unit tests ...'
+	write(*,*)
+
+	npass = 0
+	nfail = 0
+
+	call unit_test_bin_arith(npass, nfail)
+
+	write(*,*)
+	write(*,*) repeat('+', 42)
+	write(*,*) '+', npass, ' total tests passed        +'
+	write(*,*) '+', nfail, ' total tests failed        +'
+	write(*,*) repeat('+', 42)
+	write(*,*)
+	write(*,*) repeat('=', 72)
+
+end subroutine
 
 !===============================================================================
 
@@ -764,6 +853,8 @@ program main
 	write(*,*)
 	write(*,*) lang_name//' v0.0.1'
 	write(*,*)
+
+	call unit_tests()
 
 	call interpret()
 
