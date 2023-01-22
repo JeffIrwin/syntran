c202092eedf514395e2ec2913878a736c7283651
Jeff Irwin <jirwin505@gmail.com>
2023-01-22 12:11:35 -0500

    Add a fn test

--- src/tests/test.f90
+++ src/tests/test.f90
@@ -882,6 +882,41 @@ end subroutine unit_test_nd_i32
 
 !===============================================================================
 
+subroutine unit_test_fns(npass, nfail)
+
+	! More advanced tests on longer scripts
+
+	implicit none
+
+	integer, intent(inout) :: npass, nfail
+
+	!********
+
+	character(len = *), parameter :: label = 'user-defined functions'
+
+	! Path to syntran test files from root of repo
+	character(len = *), parameter :: path = 'src/tests/test-src/fns/'
+
+	logical, parameter :: quiet = .true.
+	logical, allocatable :: tests(:)
+
+	write(*,*) 'Unit testing '//label//' ...'
+
+	tests = &
+		[   &
+			interpret_file(path//'test-01.syntran', quiet) == '3', &
+			.false.  & ! so I don't have to bother w/ trailing commas
+		]
+
+	! Trim dummy false element
+	tests = tests(1: size(tests) - 1)
+
+	call unit_test_coda(tests, label, npass, nfail)
+
+end subroutine unit_test_fns
+
+!===============================================================================
+
 subroutine unit_test_array_f32_2(npass, nfail)
 
 	! More advanced tests on longer scripts
@@ -1145,6 +1180,7 @@ subroutine unit_tests(iostat)
 	call unit_test_array_f32_2(npass, nfail)
 	call unit_test_nd_i32     (npass, nfail)
 	call unit_test_intr_fns   (npass, nfail)
+	call unit_test_fns        (npass, nfail)
 
 	! TODO: add tests that mock interpreting one line at a time (as opposed to
 	! whole files)
