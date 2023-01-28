a9b7e451cc6c758abf5c3bd2a0349eca2c3aee9b
Jeff Irwin <jirwin505@gmail.com>
2023-01-27 20:16:21 -0500

    Add a test for array fn parameters

--- src/tests/test.f90
+++ src/tests/test.f90
@@ -930,6 +930,7 @@ subroutine unit_test_fns(npass, nfail)
 			interpret_file(path//'test-06.syntran', quiet) == '0', &
 			interpret_file(path//'test-07.syntran', quiet) == '0', &
 			interpret_file(path//'test-08.syntran', quiet) == 'true', &
+			interpret_file(path//'test-09.syntran', quiet) == '15', &
 			.false.  & ! so I don't have to bother w/ trailing commas
 		]
 
