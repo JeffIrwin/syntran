fd37cdf539d0e56a873160daaa9c002c7ed2b89e
Jeff Irwin <jirwin505@gmail.com>
2023-04-02 22:02:36 -0400

    update version to 0.0.18

--- src/core.f90
+++ src/core.f90
@@ -23,7 +23,7 @@ module core_m
 	integer, parameter ::   &
 		syntran_major =  0, &
 		syntran_minor =  0, &
-		syntran_patch =  17
+		syntran_patch =  18
 
 	! TODO:
 	!  - substring indexing and slicing:
