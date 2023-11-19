a7186ab35a73da926706ea5f9fdc2893a4382d24
Jeff Irwin <jirwin505@gmail.com>
2023-11-19 11:20:19 -0500

    add some hints for first time installation

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-magenta="\e[95m"
 green="\e[92;1m"
+magenta="\e[95m"
+yellow="\e[93m"
 red="\e[91;1m"
+bold="\e[;1m"
 reset="\e[0m"
 
 echo
@@ -15,9 +17,15 @@ fail_find()
 {
 	set +x
 	echo
-	echo -e "${red}Failed to find syntran${reset}"
+	echo -e "${red}Failed to find existing syntran installation${reset}"
 	echo
-	echo -e "${red}Choose a folder in your PATH environment variable and copy \"$this_syntran\" there first, before using this script to update it${reset}"
+	echo -e "${yellow}Choose a folder in your PATH environment variable and copy \"$this_syntran\" there first, before using this script to update it${reset}"
+	echo
+	echo -e "${yellow}Hint:  choose a folder such as \"/usr/local/bin/\" or \"~/bin/\", if it exists, according to your preference${reset}"
+	echo
+	echo -e "${yellow}For example:${reset}"
+	echo
+	echo -e "    ${bold}sudo cp \"$this_syntran\" /usr/local/bin/${reset}"
 	echo
 	exit -1
 }
