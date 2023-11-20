86ef86e4eef8ae8b49d159ba41e38f123dc53275
Jeff Irwin <jirwin505@gmail.com>
2023-11-19 20:28:49 -0500

    update version to 0.0.25

--- src/core.f90
+++ src/core.f90
@@ -23,7 +23,7 @@ module core_m
 	integer, parameter ::   &
 		syntran_major =  0, &
 		syntran_minor =  0, &
-		syntran_patch =  24
+		syntran_patch =  25
 
 	integer :: maxerr  ! TODO: move this (not default) into a settings struct that gets passed around
 	integer, parameter :: maxerr_def = 4
