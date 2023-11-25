03a359bce3483035c4af569141f0c5caa2b229e0
Jeff Irwin <jirwin505@gmail.com>
2023-11-24 22:33:33 -0500

    add nested #include test

--- src/tests/test.f90
+++ src/tests/test.f90
@@ -1534,6 +1534,7 @@ subroutine unit_test_include(npass, nfail)
 		[   &
 			interpret_file(path//'test-01.syntran', quiet) == '3', &
 			interpret_file(path//'test-02.syntran', quiet) == '0', &
+			interpret_file(path//'test-03.syntran', quiet) == '7', &
 			.false.  & ! so I don't have to bother w/ trailing commas
 		]
 
