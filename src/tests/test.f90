d73929650ab3d047b3040d851f5565bc44195b58
Jeff Irwin <jirwin505@gmail.com>
2024-01-02 15:57:47 -0500

    add more tests for array > operator

--- src/tests/test.f90
+++ src/tests/test.f90
@@ -1335,22 +1335,21 @@ subroutine unit_test_array_ops_1(npass, nfail)
 			eval('1 > [1, 0];') == '[false, true]', &
 			eval('1 > [0, 0];') == '[true, true]', &
 			eval('[7, 8] > [8, 7];') == '[false, true]', &
-			!! TODO: edit and uncomment remaining tests
-			!eval('[3, 2] > [3, 1];') == '[false, true]', &
-			!eval('[4, 3] > [1, 3];') == '[true, false]', &
-			!eval('[5, 6] > [1, 1];') == '[true, true]', &
-			!eval('i64(2) > [i64(0), i64(2), i64(3)];') == '[true, false, true]', &
-			!eval('[i64(0), i64(2), i64(3)] > i64(3);') == '[true, true, false]', &
-			!eval('[i64(0), i64(2), i64(3)] > [i64(4), i64(2), i64(3)];') == '[true, false, false]', &
-			!eval('2.0 > [0.0, 2.0, 3.0];') == '[true, false, true]', &
-			!eval('[0.0, 2.0, 3.0] > 3.0;') == '[true, true, false]', &
-			!eval('[0.0, 2.0, 3.0] > [4.0, 2.0, 3.0];') == '[true, false, false]', &
-			!eval('2 > [i64(0), i64(2), i64(3)];') == '[true, false, true]', &
-			!eval('[0, 2, 3] > i64(3);') == '[true, true, false]', &
-			!eval('[0, 2, 3] > [i64(4), i64(2), i64(3)];') == '[true, false, false]', &
-			!eval('i64(2) > [0, 2, 3];') == '[true, false, true]', &
-			!eval('[i64(0), i64(2), i64(3)] > 3;') == '[true, true, false]', &
-			!eval('[i64(0), i64(2), i64(3)] > [4, 2, 3];') == '[true, false, false]', &
+			eval('[3, 2] > [3, 1];') == '[false, true]', &
+			eval('[4, 3] > [1, 3];') == '[true, false]', &
+			eval('[5, 6] > [1, 1];') == '[true, true]', &
+			eval('i64(2) > [i64(0), i64(2), i64(3)];') == '[true, false, false]', &
+			eval('[i64(0), i64(2), i64(3)] > i64(0);') == '[false, true, true]', &
+			eval('[i64(4), i64(2), i64(3)] > [i64(0), i64(2), i64(3)];') == '[true, false, false]', &
+			eval('2.0 > [0.0, 2.0, 3.0];') == '[true, false, false]', &
+			eval('[0.0, 2.0, 3.0] > 2.0;') == '[false, false, true]', &
+			eval('[4.0, 2.0, 3.0] > [0.0, 2.0, 3.0];') == '[true, false, false]', &
+			eval('2 > [i64(0), i64(2), i64(3)];') == '[true, false, false]', &
+			eval('[0, 2, 3] > i64(2);') == '[false, false, true]', &
+			eval('[4, 2, 3] > [i64(0), i64(2), i64(3)];') == '[true, false, false]', &
+			eval('i64(2) > [0, 2, 3];') == '[true, false, false]', &
+			eval('[i64(0), i64(2), i64(3)] > 2;') == '[false, false, true]', &
+			eval('[i64(4), i64(2), i64(3)] > [0, 2, 3];') == '[true, false, false]', &
 			!! TODO: tests for f32 > i32, etc.
 			.false. &
 		]
