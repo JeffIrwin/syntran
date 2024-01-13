067a5cc90bf5d3f90b07277de66149c9a2de515c
Jeff Irwin <jirwin505@gmail.com>
2024-01-12 20:22:01 -0500

    refactor == operator eval as a fn

--- src/value.f90
+++ src/value.f90
@@ -123,6 +123,10 @@ module syntran__value_m
 		module procedure modulo_value_t
 	end interface mod_
 
+	interface is_eq
+		module procedure is_eq_value_t
+	end interface is_eq
+
 !===============================================================================
 
 contains
@@ -472,6 +476,338 @@ subroutine pow_value_t(left, right, res, op_text)
 	end select
 
 end subroutine pow_value_t
+
+!===============================================================================
+
+subroutine is_eq_value_t(left, right, res, op_text)
+
+	type(value_t), intent(in)  :: left, right
+
+	type(value_t), intent(inout) :: res
+
+	character(len = *), intent(in) :: op_text
+
+	!****
+
+	integer(kind = 8) :: i8
+
+	select case (magic * left%type + right%type)
+	case        (magic * i32_type + i32_type)
+		res%sca%bool = left%sca%i32 == right%sca%i32
+
+	case        (magic * i64_type + i64_type)
+		res%sca%bool = left%sca%i64 == right%sca%i64
+
+	case        (magic * i32_type + i64_type)
+		res%sca%bool = left%sca%i32 == right%sca%i64
+
+	case        (magic * i64_type + i32_type)
+		res%sca%bool = left%sca%i64 == right%sca%i32
+
+	case        (magic * f32_type + f32_type)
+		res%sca%bool = left%sca%f32 == right%sca%f32
+
+	case        (magic * f32_type + i32_type)
+		res%sca%bool = left%sca%f32 == right%sca%i32
+		! TODO: is this even possible or should I ban comparing ints and
+		! floats?  Similarly for other comparisons
+		!
+		! GNU says Warning: Equality comparison for REAL(4) at (1)
+		! [-Wcompare-reals]
+
+	case        (magic * f32_type + i64_type)
+		res%sca%bool = left%sca%f32 == real(right%sca%i64)
+
+	case        (magic * i32_type + f32_type)
+		res%sca%bool = left%sca%i32 == right%sca%f32
+
+	case        (magic * i64_type + f32_type)
+		res%sca%bool = real(left%sca%i64) == right%sca%f32
+
+	case        (magic * bool_type + bool_type)
+		res%sca%bool = left%sca%bool .eqv. right%sca%bool
+	case        (magic * str_type + str_type)
+		res%sca%bool = left%sca%str%s == right%sca%str%s
+
+	case        (magic * array_type + i32_type)
+
+		!print *, 'left%type       = ', kind_name(left%type)
+		!print *, 'left array type = ', kind_name(left%array%type)
+
+		select case (left%array%type)
+		case (i32_type)
+			res%array = mold(left%array, bool_type)
+			res%array%bool = left%array%i32 == right%sca%i32
+
+		case (i64_type)
+			res%array = mold(left%array, bool_type)
+			res%array%bool = left%array%i64 == right%sca%i32
+
+		case (f32_type)
+			res%array = mold(left%array, bool_type)
+			res%array%bool = left%array%f32 == right%sca%i32
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case        (magic * array_type + i64_type)
+
+		select case (left%array%type)
+		case (i64_type)
+			res%array = mold(left%array, bool_type)
+			res%array%bool = left%array%i64 == right%sca%i64
+
+		case (i32_type)
+			res%array = mold(left%array, bool_type)
+			res%array%bool = left%array%i32 == right%sca%i64
+
+		case (f32_type)
+			res%array = mold(left%array, bool_type)
+			res%array%bool = left%array%f32 == real(right%sca%i64)
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case        (magic * array_type + f32_type)
+
+		select case (left%array%type)
+		case (f32_type)
+			res%array = mold(left%array, bool_type)
+			res%array%bool = left%array%f32 == right%sca%f32
+
+		case (i32_type)
+			res%array = mold(left%array, bool_type)
+			res%array%bool = left%array%i32 == right%sca%f32
+
+		case (i64_type)
+			res%array = mold(left%array, bool_type)
+			res%array%bool = real(left%array%i64) == right%sca%f32
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case        (magic * array_type + bool_type)
+
+		select case (left%array%type)
+		case (bool_type)
+			res%array = mold(left%array, bool_type)
+			res%array%bool = left%array%bool .eqv. right%sca%bool
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case        (magic * array_type + str_type)
+
+		select case (left%array%type)
+		case (str_type)
+			res%array = mold(left%array, bool_type)
+
+			!! Fortran is weird about string arrays
+			!res%array%bool = left%array%str%s == right%sca%str%s
+			allocate(res%array%bool( res%array%len_ ))
+			do i8 = 1, res%array%len_
+				res%array%bool(i8) = left%array%str(i8)%s == right%sca%str%s
+			end do
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case        (magic * i32_type + array_type)
+
+		select case (right%array%type)
+		case (i32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%sca%i32 == right%array%i32
+
+		case (i64_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%sca%i32 == right%array%i64
+
+		case (f32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%sca%i32 == right%array%f32
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case        (magic * i64_type + array_type)
+
+		select case (right%array%type)
+		case (i64_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%sca%i64 == right%array%i64
+
+		case (i32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%sca%i64 == right%array%i32
+
+		case (f32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = real(left%sca%i64) == right%array%f32
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case        (magic * f32_type + array_type)
+
+		select case (right%array%type)
+		case (f32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%sca%f32 == right%array%f32
+
+		case (i32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%sca%f32 == right%array%i32
+
+		case (i64_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%sca%f32 == real(right%array%i64)
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case        (magic * bool_type + array_type)
+
+		select case (right%array%type)
+		case (bool_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%sca%bool .eqv. right%array%bool
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case        (magic * str_type + array_type)
+
+		select case (right%array%type)
+		case (str_type)
+			res%array = mold(right%array, bool_type)
+
+			! Fortran is weird about string arrays
+			allocate(res%array%bool( res%array%len_ ))
+			do i8 = 1, res%array%len_
+				res%array%bool(i8) = left%sca%str%s == right%array%str(i8)%s
+			end do
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case        (magic * array_type + array_type)
+
+		!print *, 'array == array'
+
+		select case (magic * left%array%type + right%array%type)
+		case (magic * i32_type + i32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%array%i32 == right%array%i32
+
+		case (magic * i64_type + i64_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%array%i64 == right%array%i64
+
+		case (magic * i64_type + i32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%array%i64 == right%array%i32
+
+		case (magic * i32_type + i64_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%array%i32 == right%array%i64
+
+		case (magic * f32_type + f32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%array%f32 == right%array%f32
+
+		case (magic * i32_type + f32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%array%i32 == right%array%f32
+
+		case (magic * f32_type + i32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%array%f32 == right%array%i32
+
+		case (magic * i64_type + f32_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = real(left%array%i64) == right%array%f32
+
+		case (magic * f32_type + i64_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%array%f32 == real(right%array%i64)
+
+		case (magic * bool_type + bool_type)
+			res%array = mold(right%array, bool_type)
+			res%array%bool = left%array%bool .eqv. right%array%bool
+
+		case (magic * str_type + str_type)
+			res%array = mold(right%array, bool_type)
+
+			! Fortran is weird about string arrays
+			allocate(res%array%bool( res%array%len_ ))
+			do i8 = 1, res%array%len_
+				res%array%bool(i8) = left%array%str(i8)%s == right%array%str(i8)%s
+			end do
+
+		case default
+			write(*,*) err_eval_binary_types(op_text)
+			call internal_error()
+		end select
+
+	case default
+		! FIXME: other numeric types (f64, etc.)
+		write(*,*) err_eval_binary_types(op_text)
+		call internal_error()
+	end select
+
+end subroutine is_eq_value_t
+
+!===============================================================================
+
+function mold(mold_, type_) result(array)
+
+	! Construct array meta-data, such as type, rank, and size, based on a given
+	! mold
+	!
+	! The actual allocation of array%i32 or array%bool (appropriately depending
+	! on the type) and setting of its values is done outside of here in the
+	! calling fn
+
+	type(array_t), intent(in) :: mold_
+	!type(value_t), intent(in) :: mold_
+
+	integer, intent(in) :: type_
+
+	type(array_t), allocatable :: array
+
+	allocate(array)
+
+	array%type = type_
+
+	!array%kind = expl_array
+	array%rank = mold_%rank
+
+	array%len_ = mold_%len_
+	array%cap  = mold_%cap
+	array%size = mold_%size
+
+end function mold
 
 !===============================================================================
 
