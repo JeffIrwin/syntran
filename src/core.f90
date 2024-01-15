15bddf4256678890e083f6b7a495d0ac9100a4a7
Jeff Irwin <jirwin505@gmail.com>
2024-01-15 12:22:11 -0500

    update version to 0.0.40

--- src/core.f90
+++ src/core.f90
@@ -25,7 +25,7 @@ module syntran__core_m
 	integer, parameter ::   &
 		syntran_major =  0, &
 		syntran_minor =  0, &
-		syntran_patch =  39
+		syntran_patch =  40
 
 	! TODO:
 	!  - refactor syntrax_eval() deep nesting
