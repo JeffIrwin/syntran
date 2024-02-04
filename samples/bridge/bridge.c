aaecdeb6a851c158635908ab3632b235de60e112
Jeff Irwin <jirwin505@gmail.com>
2024-02-03 21:32:16 -0500

    mvp c: memset is driving me fucking nuts

--- samples/bridge/bridge.c
+++ samples/bridge/bridge.c
@@ -6,7 +6,7 @@
 
 //==============================================================================
 
-#define IFILE 0
+#define IFILE 5
 
 #if IFILE == 0
 	char* filename_glbl = "samples/bridge/test-input-0.txt";
@@ -24,6 +24,8 @@
 
 #define NNAMES_CAP 4 * 1024  // 1493 for my input
 #define NAME_CAP 8           // max name length
+
+#define NADJ_CAP 16
 
 char names_glbl[NNAMES_CAP][NAME_CAP];
 //char* names_glbl[NNAMES_CAP];
@@ -49,7 +51,7 @@ void println(char* str)
 void push_name(char* name_)
 {
 	//if (name_ == "") return;
-	printf("name_ = \"%s\"\n", name_);
+	//printf("name_ = \"%s\"\n", name_);
 
 	if (strlen(name_) >= NAME_CAP)
 	{
@@ -101,8 +103,44 @@ void push_name(char* name_)
 
 //==============================================================================
 
+int lookup_name(char* name_)
+{
+	int i = 0;
+	bool done_search = false;
+	while (!done_search)
+	{
+		if (i >= NNAMES_CAP)
+		{
+			println("\nError: NNAMES_CAP overflow!\n");
+			exit(-1);
+		}
+
+		if (name_ids[i] < 0)
+		{
+			println("\nError: name lookup failed!\n");
+			exit(-1);
+		}
+		else
+		{
+			if (strcmp(name_, names_glbl[i]) == 0)
+				done_search = true;
+			else if (strcmp(name_, names_glbl[i]) < 0)
+				// Iterate into left sub-tree
+				i = left[i];
+			else // greater than
+				// Iterate into right sub-tree
+				i = right[i];
+		}
+	}
+	return name_ids[i];
+}
+
+//==============================================================================
+
 //let g_glbl = [0; 0, 0];
-//let num_adj_glbl = [0; 0];
+//int** g_glbl;
+int* g_glbl;
+int* num_adj_glbl;
 void read_aoc_graph(char* filename)
 {
 	// First pass: save list of unique names
@@ -122,10 +160,11 @@ void read_aoc_graph(char* filename)
 		perror("Error opening file");
 		exit(-1);
 	}
+	char* dlms = ": \n";
+
 	while (fgets(str_, MAX_LEN, f) != NULL)
 	{
 		//println(str_);
-		char* dlms = ": \n";
 		char* substr = strtok(str_, dlms);
 		while (substr != NULL)
 		{
@@ -133,77 +172,256 @@ void read_aoc_graph(char* filename)
 			substr = strtok(NULL, dlms);
 		}
 	}
-	printf("Number of nodes = %d\n", num_nodes);
-	for (int i = 0; i < num_nodes; i++)
-		printf("\"%s\"\n", names_glbl[i]);
-	printf("name_ids = \n");
-	for (int i = 0; i < num_nodes; i++)
-		printf("%d  ", name_ids[i]);
-	printf("\n");
-
-	fclose(f);
-
-	/*
+	//printf("Number of nodes = %d\n", num_nodes);
+	//for (int i = 0; i < num_nodes; i++)
+	//	printf("\"%s\"\n", names_glbl[i]);
+	//printf("name_ids = \n");
+	//for (int i = 0; i < num_nodes; i++)
+	//	printf("%d  ", name_ids[i]);
+	//printf("\n");
 
 	// Second pass: save one-way directed connections as graph gdir
-	//
-	// TODO: 2nd pass could probably be optimized by not using lookup_name().
-	// Need to return id from push_name() and save it somewhere in 1st pass
-	let NADJ_CAP = 16;
-	let gdir = [-1; NADJ_CAP, num_nodes];
-	let num_adjdir = [0; num_nodes];
-	close(f);
 	println("2nd pass ...");
-	f = open(filename);
-	str_ = readln(f);
-	while not eof(f)
+
+	//int gdir[num_nodes][NADJ_CAP];
+	int gdir[num_nodes * NADJ_CAP];
+
+	memset(gdir, -1, sizeof(gdir));
+
+	int num_adjdir[num_nodes];
+	memset(num_adjdir, 0, sizeof(num_adjdir));
+
+	rewind(f);
+	while (fgets(str_, MAX_LEN, f) != NULL)
 	{
-		//println("str_ = ", str_);
+		//println(str_);
+		char* substr = strtok(str_, dlms);
+		int from = lookup_name(substr);
+		//printf("name, id = %s, %d\n", substr, from);
 
-		let strs = split_(str_, ": ");
-		let from = lookup_name(strs[0]);
-		for i in [1: size(strs, 0)]
+		substr = strtok(NULL, dlms);
+		while (substr != NULL)
 		{
-			let to = lookup_name(strs[i]);
+			int to = lookup_name(substr);
+			//printf("to = %d\n", to);
 
-			if num_adjdir[from] > NADJ_CAP
+			if (num_adjdir[from] > NADJ_CAP)
 			{
-				println("");
-				println("Error: NADJ_CAP overflow 1!");
-				println("");
+				printf("\nError: NADJ_CAP overflow 1!\n");
 				exit(-1);
 			}
 
-			gdir[ num_adjdir[from], from ] = to;
+			//gdir[ num_adjdir[from], from ] = to;
+
+			//gdir[from][ num_adjdir[from] ] = to;
+			gdir[ from * NADJ_CAP + num_adjdir[from] ] = to;
+
 			num_adjdir[from] += 1;
+
+			substr = strtok(NULL, dlms);
 		}
-
-		str_ = readln(f);
 	}
-	//println("gdir = ", gdir);
+	//printf("num_adjdir = \n");
+	//for (int i = 0; i < num_nodes; i++)
+	//	printf("%d  ", num_adjdir[i]);
+	//printf("\n");
 
-	g_glbl = gdir;
-	num_adj_glbl = num_adjdir;
+	//printf("gdir = \n");
+	//for (int i = 0; i < num_nodes; i++)
+	//{
+	//	for (int j = 0; j < num_adjdir[i]; j++)
+	//	{
+	//		//printf("%d  ", gdir[i][j]);
+	//		printf("%d  ", gdir[i * NADJ_CAP + j]);
+	//	}
+	//	printf("\n");
+	//}
+	//printf("\n");
+
+	g_glbl = malloc(num_nodes * NADJ_CAP * sizeof(int));
+	memcpy(g_glbl, gdir, sizeof(gdir));
+
+	num_adj_glbl = malloc(num_nodes * sizeof(int));
+	memcpy(num_adj_glbl, num_adjdir, sizeof(num_adjdir));
 
 	// Double each edge to convert the directed graph to an undirected graph
-	for i in [0: num_nodes]
-	for j in [0: num_adjdir[i]]
+	for (int i = 0; i < num_nodes; i++)
+	for (int j = 0; j < num_adjdir[i]; j++)
 	{
-		let from = i;
-		let to   = gdir[j, i];
-		if num_adj_glbl[to] > NADJ_CAP
+		int from = i;
+		//int to   = gdir[j, i];
+		int to   = gdir[i * NADJ_CAP + j];
+		//printf("from, to = %d, %d\n", from, to);
+
+		if (num_adj_glbl[to] > NADJ_CAP)
 		{
-			println("");
-			println("Error: NADJ_CAP overflow 2!");
-			println("");
+			println("\nError: NADJ_CAP overflow 2!\n");
 			exit(-1);
 		}
-		g_glbl[ num_adj_glbl[to], to ] = from;
+		//g_glbl[ num_adj_glbl[to], to ] = from;
+		g_glbl[ to * NADJ_CAP + num_adj_glbl[to] ] = from;
+
 		num_adj_glbl[to] += 1;
 	}
 	//println("g_glbl = ", g_glbl);
-	//println("num_adj_glbl = ", num_adj_glbl);
-	*/
+
+	printf("num_adj_glbl = \n");
+	for (int i = 0; i < num_nodes; i++)
+		printf("%d  ", num_adj_glbl[i]);
+	printf("\n");
+
+	printf("g_glbl = \n");
+	for (int i = 0; i < num_nodes; i++)
+	{
+		for (int j = 0; j < num_adj_glbl[i]; j++)
+			printf("%d  ", g_glbl[i * NADJ_CAP + j]);
+		printf("\n");
+	}
+	printf("\n");
+
+	fclose(f);
+}
+
+//==============================================================================
+
+int min(int a, int b)
+{
+	return a < b ? a : b;
+}
+
+//********
+
+//int* get_bridge(g: [i32; :, :], num_adj: [i32; :])
+//int* get_bridge(int* g, int* num_adj)
+int* get_bridge(int* g, int* num_adj, int nn)
+{
+	// Search for a bridge in graph g in the component connected to node 0.
+	// Return the pair of nodes that make up the first bridge found, or [-1, -1]
+	// if no bridges are found
+
+	//printf("g = \n");
+	//for (int i = 0; i < nn; i++)
+	//{
+	//	for (int j = 0; j < num_adj[i]; j++)
+	//		printf("%d  ", g[i * NADJ_CAP + j]);
+	//	printf("\n");
+	//}
+	//printf("\n");
+
+	//int nn = size(num_adj, 0);
+
+	// Stack for iterative (non-recursive) depth-first search
+	int STACK_CAP = 16 * nn;
+	int* stack = malloc(STACK_CAP * sizeof(int));
+	memset(stack, 0, sizeof(stack));
+	int sp = -1; // stack "pointer"
+
+	// Push node 0 as root
+	//stack[(sp += 1)] = 0;
+	stack[++sp] = 0;
+
+	bool* visited = malloc(nn * sizeof(bool));
+	bool* defer   = malloc(nn * sizeof(bool));
+	int*  parent  = malloc(nn * sizeof(int));
+	int*  low     = malloc(nn * sizeof(int));
+	int*  dists   = malloc(nn * sizeof(int));
+
+	//// idk why the first memset doesn't work :shrug:
+	//memset(visited, false, sizeof(visited));
+	//memset(defer  , false, sizeof(defer  ));
+	memset(visited, false, nn * sizeof(bool));
+	memset(defer  , false, nn * sizeof(bool));
+
+	memset(parent , -1   , sizeof(parent ));
+	memset(low    , -1   , sizeof(low    ));
+	memset(dists  , -1   , sizeof(dists  ));
+
+	int loop_count = 0;
+
+	int dist = 0;
+	bool found = false;
+	//int bridge[2]; bridge[0] = -1; bridge[1] = -1;
+	int* bridge = malloc(2 * sizeof(int)); bridge[0] = -1; bridge[1] = -1;
+	while (sp >= 0 && !found)
+	{
+		loop_count++;
+		//if (loop_count > 3) exit(0);
+
+		//int v = stack[(sp -= 1) + 1]; // pop
+		int v = stack[sp--]; // pop
+
+		printf("\n");
+		printf("v = %d\n", v);
+
+		if (!visited[v])
+		{
+			low[v]   = dist;
+			dists[v] = dist;
+			dist += 1;
+		}
+		visited[v] = true;
+
+		//for iw in [0: num_adj[v]]
+		for (int iw = 0; iw < num_adj[v]; iw++)
+		{
+			//int w = g[iw, v];
+			int w = g[v * NADJ_CAP + iw];
+			printf("w = %d\n", w);
+
+			if (w == parent[v])
+			{
+				// Do nothing
+				printf("parent\n");
+			}
+			else if (visited[w])
+			{
+				low[v] = min(low[v], dists[w]);
+				printf("visited\n");
+			}
+			else
+			{
+				parent[w] = v;
+
+				printf("pushing %d\n", v);
+				stack[++sp] = v;  // re-push parent for deferred processing
+				//check_stack_cap(sp, STACK_CAP); // TODO
+				defer[v] = true;
+
+				printf("pushing %d\n", w);
+				stack[++sp] = w; // push child. order matters wrt parent
+				//check_stack_cap(sp, STACK_CAP); // TODO
+			}
+		}
+
+		if (defer[v])
+		{
+			//for iw in [0: num_adj[v]]
+			for (int iw = 0; iw < num_adj[v]; iw++)
+			{
+				//int w = g[iw, v];
+				int w = g[v * NADJ_CAP + iw];
+
+				if ((w != parent[v]) && visited[w])
+				{
+					low[v] = min(low[v], low[w]);
+					if (low[w] > dists[v])
+					{
+						//println("Found bridge = ", [v, w], " = ", [names_glbl[v], names_glbl[w]]);
+						printf("Found bridge = [%d, %d]", v, w);
+						//bridge = [v, w];
+						bridge[0] = v; bridge[1] = w;
+						found = true;
+					}
+				}
+			}
+		}
+	}
+	//println("visited = ", visited);
+	//println("parent = ", parent);
+	//println("dists    = ", dists);
+	//println("low  = ", low);
+
+	return bridge;
 }
 
 //==============================================================================
@@ -216,14 +434,18 @@ int main()
 	read_aoc_graph(filename_glbl);
 
 	println("Running C Tarjan's bridge algorithm ...");
-	//let bridge = get_bridge(g_glbl, num_adj_glbl);
-	//if bridge[0] < 0
-	//	println("No bridges were found");
-	//else
-	//{
-	//	println("Found bridge between nodes ", bridge, " = ",
-	//			[names_glbl[bridge[0]], names_glbl[bridge[1]]]);
-	//}
+	int* bridge = get_bridge(g_glbl, num_adj_glbl, num_nodes);
+	if (bridge[0] < 0)
+		println("No bridges were found");
+	else
+	{
+		//println("Found bridge");
+		printf("Found bridge between nodes [%d, %d] = [%s, %s]\n",
+				bridge[0], bridge[1],
+				names_glbl[bridge[0]], names_glbl[bridge[1]]);
+		//println("Found bridge between nodes ", bridge, " = ",
+		//		[names_glbl[bridge[0]], names_glbl[bridge[1]]]);
+	}
 
 	println("Ending C Tarjan's bridge sample");
 	println("");
@@ -231,42 +453,6 @@ int main()
 }
 
 /*
-
-fn lookup_name(name_: str): i32
-{
-	let i = 0;
-	let done_search = false;
-	while not done_search
-	{
-		if i >= NNAMES_CAP
-		{
-			println("");
-			println("Error: NNAMES_CAP overflow!");
-			println("");
-			exit(-1);
-		}
-
-		if name_ids[i] < 0
-		{
-			println("");
-			println("Error: name lookup failed!");
-			println("");
-			exit(-1);
-		}
-		else
-		{
-			if names_glbl[i] == name_
-				done_search = true;
-			else if is_str_lt(name_, names_glbl[i])
-				// Iterate into left sub-tree
-				i = left[i];
-			else // greater than
-				// Iterate into right sub-tree
-				i = right[i];
-		}
-	}
-	let ans = name_ids[i];
-}
 
 //==============================================================================
 
@@ -279,94 +465,6 @@ fn check_stack_cap(n: i32, cap: i64)
 		println("");
 		exit(-1);
 	}
-}
-
-//==============================================================================
-
-fn get_bridge(g: [i32; :, :], num_adj: [i32; :]): [i32; :]
-{
-	// Search for a bridge in graph g in the component connected to node 0.
-	// Return the pair of nodes that make up the first bridge found, or [-1, -1]
-	// if no bridges are found
-
-	let nn = size(num_adj, 0);
-
-	// Stack for iterative (non-recursive) depth-first search
-	let STACK_CAP = 16 * nn;
-	let stack = [0; STACK_CAP];
-	let sp = -1; // stack "pointer"
-
-	// Push node 0 as root
-	stack[(sp += 1)] = 0;
-
-	let visited = [false; nn];
-	let defer   = [false; nn];
-	let parent  = [-1   ; nn];
-	let low	    = [-1   ; nn];
-	let dists   = [-1   ; nn];
-
-	let dist = 0;
-	let found = false;
-	let bridge = [-1, -1];
-	while sp >= 0 and not found
-	{
-		let v = stack[(sp -= 1) + 1]; // pop
-		if not visited[v]
-		{
-			low[v]   = dist;
-			dists[v] = dist;
-			dist += 1;
-		}
-		visited[v] = true;
-
-		for iw in [0: num_adj[v]]
-		{
-			let w = g[iw, v];
-			if w == parent[v]
-			{
-				// Do nothing
-			}
-			else if visited[w]
-			{
-				low[v] = min(low[v], dists[w]);
-			}
-			else
-			{
-				parent[w] = v;
-
-				stack[(sp += 1)] = v;  // re-push parent for deferred processing
-				check_stack_cap(sp, STACK_CAP);
-				defer[v] = true;
-
-				stack[(sp += 1)] = w; // push child. order matters wrt parent
-				check_stack_cap(sp, STACK_CAP);
-			}
-		}
-
-		if defer[v]
-		{
-			for iw in [0: num_adj[v]]
-			{
-				let w = g[iw, v];
-				if w != parent[v] and visited[w]
-				{
-					low[v] = min(low[v], low[w]);
-					if (low[w] > dists[v])
-					{
-						//println("Found bridge = ", [v, w], " = ", [names_glbl[v], names_glbl[w]]);
-						bridge = [v, w];
-						found = true;
-					}
-				}
-			}
-		}
-	}
-	//println("visited = ", visited);
-	//println("parent = ", parent);
-	//println("dists    = ", dists);
-	//println("low  = ", low);
-
-	let ans = bridge;
 }
 
 //==============================================================================
