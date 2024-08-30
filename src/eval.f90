08cac79b90e316dc02602bb4a30146cad1c2cbe1
Jeff Irwin <jirwin505@gmail.com>
2024-08-30 19:31:45 -0400

    use recursion instead of iteration for rhs dot expr eval

--- src/eval.f90
+++ src/eval.f90
@@ -402,6 +402,11 @@ subroutine eval_dot_expr(node, state, res)
 	! This won't work for struct literal member access.  It only works for
 	! `identifier.member`
 
+	res = get_val(node, state%vars%vals(node%id_index))
+	return
+
+	! TODO: delete
+
 	! In nested expressions, like `a.b.c.d`, val begins as the top-most
 	! (left-most, outer-most) value `a`
 	val = state%vars%vals(node%id_index)
@@ -421,6 +426,17 @@ subroutine eval_dot_expr(node, state, res)
 
 		! Descend the val from `a` to `b`, etc.  A temporary value is needed
 		! because copy assignment is a complex overridden routine
+		!
+		! I think LHS dot assignment will need to traverse the `a.b.c.d` tree
+		! twice, or in a loop.  First descend from a to d, and ascend back up
+		! from d back to a to finally assign the outermost struct
+		!
+		! Actually, a better way to handle dot exprs on both sides is with
+		! recursive getter and setter fn's.  These would avoid issues with
+		! copying and having to double walk the tree.  Here, use a getter.  In
+		! LHS dot exprs, first use a getter, then do compound assignment, and
+		! finally use a setter.  This will act similarly to get_array_value_t()
+		! and set_array_value_t()
 		tmp_val = val%struct(id)
 		val = tmp_val
 
@@ -436,6 +452,35 @@ subroutine eval_dot_expr(node, state, res)
 	!print *, "struct[", str(i), "] = ", state%vars%vals(node%id_index)%struct(i)%to_str()
 
 end subroutine eval_dot_expr
+
+!===============================================================================
+
+recursive function get_val(node, val) result(res)
+
+	type(syntax_node_t), intent(in) :: node
+	type(value_t), intent(in) :: val
+
+	!type(value_t), intent(inout) :: res
+	type(value_t) :: res
+
+	!********
+	!type(value_t) :: val, tmp_val
+	!type(syntax_node_t) :: mem, tmp_node
+	integer :: id
+
+	id = node%member%id_index
+
+	if (node%member%kind == dot_expr) then
+		! Recurse
+		res = get_val(node%member, val%struct(id))
+		return
+	end if
+
+	! Base case
+	res = val%struct(id)
+	!res = val%struct(node%member%id_index)
+
+end function get_val
 
 !===============================================================================
 
