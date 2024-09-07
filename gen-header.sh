8ea864b55a196650c96f0df810b39abfb8e821c2
Jeff Irwin <jirwin505@gmail.com>
2024-09-07 19:56:26 -0400

    macos is the absolute worst

--- gen-header.sh
+++ gen-header.sh
@@ -5,7 +5,10 @@ set -exu
 hash=$(git rev-parse --short HEAD)
 header=./src/compiler.F90
 
-sed -i \
+# `sed -i` doesn't fucking work on macos so we have to pipe to a temp file and
+# then move it
+sed \
 	's/\(git_commit\s*=\s*\)".*"/\1"'$hash'"/' \
-	"$header"
+	"$header" > "$header.sed"
+mv "$header.sed" "$header"
 
