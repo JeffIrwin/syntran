89319a8914d87f6aa0bc3797525cb218267f38d7
Jeff Irwin <jirwin505@gmail.com>
2024-12-15 19:11:59 -0500

    update version to 0.0.58

--- src/core.f90
+++ src/core.f90
@@ -27,7 +27,7 @@ module syntran__core_m
 	integer, parameter ::   &
 		syntran_major =  0, &
 		syntran_minor =  0, &
-		syntran_patch =  57
+		syntran_patch =  58
 
 	! TODO:
 	!  - ban expression statements?
