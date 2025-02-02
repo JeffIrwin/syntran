d0ce897dbeaa1e470d1cb5416ef98fdef7846c8f
Jeff Irwin <jirwin505@gmail.com>
2025-02-01 19:54:03 -0500

    test syntran explorer in release profile too

--- test-explorer.sh
+++ test-explorer.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -exu
 
+# TODO: take a 'profile' arg (and other args?) here, and then DRY up this script
+# with its copies in main.yml
+#
+# Be careful not to trigger a re-compile of release mode in ci/cd due to 1
+# differring arg.  Maybe just pass along $*
+
 dir_="src/tests/test-src/explorer/"
 
 for f in $dir_/*.syntran ; do
