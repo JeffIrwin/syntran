d7673331338aa87fa3f0c03009944103970112a7
Jeff Irwin <jirwin505@gmail.com>
2025-02-05 19:03:59 -0500

    test alma linux

---
+++ test-branch.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -uxe
+GREEN="\033[1;32m"
+RESET="\033[0m"
+
+pushd docker
+files=Dockerfile.branch-*
+
+docker_args=""
+#docker_args+=" --no-cache"
+
+ntest=0
+
+for file in ${files[@]}; do
+	echo "file = $file"
+	(( ntest += 1 ))
+
+	sudo docker build . \
+		$docker_args \
+		--file "$file"
+
+done
+
+popd  # from docker
+
+echo -e $GREEN"Successfully finished $ntest tests!"$RESET
+
