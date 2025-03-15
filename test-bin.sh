0df0076cd6a9dd164148110a2dbb410a16300c37
Jeff Irwin <jirwin505@gmail.com>
2025-03-14 22:12:21 -0400

    retry flaky docker tests

--- test-bin.sh
+++ test-bin.sh
@@ -12,11 +12,37 @@ docker_args=""
 
 ntest=0
 
+retry()
+{
+	# Retries a command on failure.
+	# $1 - the max number of attempts
+	# $2... - the command to run
+	#
+	# Source:
+	#
+	#     https://stackoverflow.com/a/35977896/4347028
+
+	local -r -i max_attempts="$1"; shift
+	local -r cmd="$@"
+	local -i attempt_num=1
+
+	until $cmd
+		do
+			if (( attempt_num == max_attempts )) ; then
+				echo "Attempt $attempt_num failed and there are no more attempts left!"
+				return 1
+			else
+				echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
+				sleep $(( attempt_num++ ))
+			fi
+	done
+}
+
 for file in ${files[@]}; do
 	echo "file = $file"
 	(( ntest += 1 ))
 
-	sudo docker build . \
+	retry 3 sudo docker build . \
 		$docker_args \
 		--file "$file"
 
