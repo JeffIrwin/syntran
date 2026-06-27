JeffIrwin/syntran

commit 7fd607e2c4907ac0278f009d3542e583b306d763

allow long chains of array/struct accessors with methods
array subscripts and child accessors can be chained indefinitely, but
method calls can only currently appear at the end of the chain

this is similar to old syntran behavior, i.e. a fn returning an array
cannot be subscripted directly

src/tests/test.f90
@@ -4843,6 +4843,56 @@ subroutine unit_test_methods(npass, nfail)
 			eval(CTR//'let a=[C{n=5}]; a[0].get();', quiet) == '5', &  ! 19 (const method on subscripted element)
 			diag_has_code(get_diags(CTR//'const a=[C{n=0}]; a[0].inc();'), EC_CONST_ASSIGN), &  ! 20
 
+			! --- method at end of deep struct chain (no arrays) ---
+			eval('' &                                                                    ! 21
+				//'struct A{a:i32, const fn get():i32{return a;}}' &
+				//'struct B{b:A} struct C{c:B}' &
+				//'let sa=A{a=42}; let sb=B{b=sa}; let sc=C{c=sb};' &
+				//'return sc.c.b.get();' &
+				, quiet) == '42', &
+			eval('' &                                                                    ! 22
+				//'struct A{a:i32, const fn get():i32{return a;}}' &
+				//'struct B{b:A} struct C{c:B} struct D{d:C} struct E{e:D}' &
+				//'let sa=A{a=42}; let sb=B{b=sa}; let sc=C{c=sb};' &
+				//'let sd=D{d=sc}; let se=E{e=sd};' &
+				//'return se.e.d.c.b.get();' &
+				, quiet) == '42', &
+
+			! --- const method at end of chain with alternating subscripts ---
+			eval('' &                                                                    ! 23
+				//'struct A{a:[i32;:], const fn first():i32{return a[0];}}' &
+				//'struct B{b:A} struct C{c:[B;:]} struct D{d:C} struct E{e:[D;:]}' &
+				//'let sa=A{a=[42]}; let sb=B{b=sa}; let sc=C{c=[sb]};' &
+				//'let sd=D{d=sc}; let se=E{e=[sd]};' &
+				//'return se.e[0].d.c[0].b.first();' &
+				, quiet) == '42', &
+
+			! --- mutable method called deep in chain ---
+			eval('' &                                                                    ! 24
+				//'struct A{a:i32, fn inc(){a+=1;} const fn get():i32{return a;}}' &
+				//'struct B{b:A} struct C{c:B}' &
+				//'let sa=A{a=41}; let sb=B{b=sa}; let sc=C{c=sb};' &
+				//'sc.c.b.inc();' &
+				//'return sc.c.b.get();' &
+				, quiet) == '42', &
+			eval('' &                                                                    ! 25
+				//'struct A{a:i32, fn inc(){a+=1;} const fn get():i32{return a;}}' &
+				//'struct B{b:A} struct C{c:[B;:]} struct D{d:C} struct E{e:[D;:]}' &
+				//'let sa=A{a=41}; let sb=B{b=sa}; let sc=C{c=[sb]};' &
+				//'let sd=D{d=sc}; let se=E{e=[sd]};' &
+				//'se.e[0].d.c[0].b.inc();' &
+				//'return se.e[0].d.c[0].b.get();' &
+				, quiet) == '42', &
+
+			! --- subscripted receiver inside a wrapped struct ---
+			eval('' &                                                                    ! 26
+				//'struct A{a:i32, const fn get():i32{return a;}}' &
+				//'struct B{b:[A;:]} struct C{c:B}' &
+				//'let b=B{b=[A{a=10},A{a=42}]};' &
+				//'let c=C{c=b};' &
+				//'return c.c.b[1].get();' &
+				, quiet) == '42', &
+
 			.false. &
 		]
 

src/compile_ctrl.f90
@@ -796,8 +796,9 @@ recursive subroutine compile_node(prog, cs, node)
 				! Pass 2: move by-ref args from their variable slots onto stack.
 				do i = 1, size(node%is_ref)
 					if (node%is_ref(i)) then
-						if (allocated(node%args(i)%lsubscripts)) then
-							! Subscripted receiver: push element value (OP_INDEX).
+						if (allocated(node%args(i)%lsubscripts) .or. &
+						    node%args(i)%kind == dot_expr) then
+							! Subscripted or dot-expr receiver: evaluate to get value.
 							call compile_node(prog, cs, node%args(i))
 						else if (node%args(i)%is_loc) then
 							call emit(prog, OP_LOAD_REF_LOCAL,  a = node%args(i)%id_index)

src/core.f90
@@ -34,6 +34,10 @@ module syntran__core_m
 	!  - method improvements:
 	!    * allow subscripted methods: `struct[0].method()`
 	!    * other chained permutations like above?
+	!    * method calls only work at the *end* of a chain. this is consistent
+	!      with older syntran behavior, like a fn return an array cannot be
+	!      subscripted directly -- it needs to go into an intermediate variable
+	!      first. should these features be extended?
 	!    * lump in other struct improvement unrelated to methods, e.g.: struct
 	!      array slicing, noted below
 	!    * DRY shared code between methods and fns, e.g. argument count and type

src/parse_expr.f90
@@ -896,7 +896,7 @@ recursive module subroutine parse_dot(parser, expr)
 
 	type(struct_t) :: struct
 
-	type(syntax_node_t) :: receiver_save, arg_
+	type(syntax_node_t) :: receiver_save, arg_, receiver_cand, method_cand
 
 	type(syntax_node_vector_t) :: call_args
 
@@ -1168,8 +1168,27 @@ recursive module subroutine parse_dot(parser, expr)
 	if (parser%peek_kind(0) == dot_token) then
 		expr%member%val = expr%val
 		expr%member%identifier = identifier  ! set for diags in recursed parse_dot()
+
+		! Save the current expr (e.g. y.b) before the recursive call can turn
+		! expr%member into a method_call_expr.  Used to build the proper receiver
+		! if a method is found deeper in the chain.
+		receiver_cand = expr
+
 		call parser%parse_dot(expr%member)
 		expr%val = expr%member%val
+
+		! If the recursive call resolved a method call, restructure so the
+		! full chain (receiver_cand) is the method's receiver rather than just
+		! the innermost sub-node.
+		if (expr%member%kind == method_call_expr) then
+			! receiver_cand is the outer dot_expr (e.g. y.b).  Its member
+			! should be the inner partial chain produced by the recursive fix
+			! (rather than the b-subnode copy from the save above).
+			receiver_cand%member = expr%member%args(1)
+			method_cand          = expr%member        ! deep-copy the method node
+			method_cand%args(1)  = receiver_cand      ! replace receiver with full chain
+			expr                 = method_cand         ! promote to top-level method call
+		end if
 	end if
 
 end subroutine parse_dot

src/vm_exec.f90
@@ -666,8 +666,9 @@ module subroutine vm_run(prog, state, res)
 			! Write by-ref modified values back to caller's variable slots.
 			do i = 1, nparams
 				if (.not. cn%is_ref(i)) cycle
-				if (allocated(cn%args(i)%lsubscripts)) then
-					! Subscripted receiver: write element back via set_val.
+				if (allocated(cn%args(i)%lsubscripts) .or. &
+				    cn%args(i)%kind == dot_expr) then
+					! Subscripted or dot-expr receiver: write element back via set_val.
 					if (cn%args(i)%is_loc) then
 						call set_val(cn%args(i), state%locs%vals(cn%args(i)%id_index), state, params_pool(i))
 					else

commit d1c5f687900262badfb2ef22959f038deeefa68b

fix ast backend broken in commit 7fd607e2
also add --no-warn-ast option to quiet warnings for unit tests in ci/cd

.github/workflows/main.yml
@@ -107,7 +107,7 @@
 					fpm test test --profile debug --link-flag \"$lflags\" \n
 
 					# Also test the legacy AST backend so both paths are exercised \n
-					SYNTRAN_BACKEND=ast fpm test test --profile debug --link-flag \"$lflags\" \n
+					SYNTRAN_BACKEND=ast fpm test test --profile debug --link-flag \"$lflags\" -- --no-warn-ast \n
 				"
 			},
 			{

src/consts.f90
@@ -11,6 +11,8 @@ module syntran__consts_m
 
 	logical :: permissive_return = .false.
 
+	logical :: no_warn = .false.
+
 	! Initial capacity of scope dict pointer arrays.  They are dynamic arrays of
 	! pointers, so they take little memory to begin with, and then grow
 	! dynamically.  Hence, I have no idea what the default should be and it

src/eval_fn.f90
@@ -115,6 +115,9 @@ recursive module subroutine eval_fn_call(node, state, res)
 			else if (allocated(node%args(i)%lsubscripts)) then
 				! Subscripted receiver (e.g. arr[0].method()): copy element out
 				call syntax_eval(node%args(i), state, params_tmp(i))
+			else if (node%args(i)%kind == dot_expr) then
+				! Dot-chain receiver (e.g. sc.c.b): evaluate chain to extract inner struct
+				call syntax_eval(node%args(i), state, params_tmp(i))
 			else if (node%args(i)%is_loc) then
 				call value_move(state%locs%vals( node%args(i)%id_index ), params_tmp(i))
 			else
@@ -177,6 +180,13 @@ recursive module subroutine eval_fn_call(node, state, res)
 
 		if (allocated(node%args(i)%lsubscripts)) then
 			! Subscripted receiver: write modified element back
+			if (node%args(i)%is_loc) then
+				call set_val(node%args(i), state%locs%vals( node%args(i)%id_index ), state, params_tmp(i))
+			else
+				call set_val(node%args(i), state%vars%vals( node%args(i)%id_index ), state, params_tmp(i))
+			end if
+		else if (node%args(i)%kind == dot_expr) then
+			! Dot-chain receiver: write modified struct back through the chain
 			if (node%args(i)%is_loc) then
 				call set_val(node%args(i), state%locs%vals( node%args(i)%id_index ), state, params_tmp(i))
 			else

src/syntran.f90
@@ -479,7 +479,7 @@ subroutine init_state(state, script_args, src_dir, bytecode)
 		if (backend_status == 0) then
 			if (trim(backend_env) == "ast") then
 				state%bytecode = .false.
-				write(error_unit, '(a)') fg_bold_yellow//'Warning'//color_reset// &
+				if (.not. no_warn) write(error_unit, '(a)') fg_bold_yellow//'Warning'//color_reset// &
 					': SYNTRAN_BACKEND=ast is deprecated. ' // &
 					'The AST walker will be removed in a future release. If you encounter ' // &
 					'bugs, please report them at https://github.com/JeffIrwin/syntran/issues'

src/tests/test.f90
@@ -6772,12 +6772,21 @@ end module test_m
 program test
 
 	use syntran__app_m
+	use syntran__consts_m
 	use test_m
 	implicit none
 
-	integer :: io
+	integer :: i, argc, io
+	character(len = 256) :: argv
 
 	call set_ansi_colors(.true.)
+
+	argc = command_argument_count()
+	do i = 1, argc
+		call get_command_argument(i, argv)
+		if (trim(argv) == '--no-warn-ast') no_warn = .true.
+	end do
+
 	call unit_tests(io)
 	call exit(io)
 