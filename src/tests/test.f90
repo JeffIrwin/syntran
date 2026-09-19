f3e9e007b6b062c05a0eb52db4acfbfff3e71708
Jeff Irwin <jirwin505@gmail.com>
2026-09-19 17:13:20 -0400

    add newton-raphson/LU unit test from aoc repo

--- src/tests/test.f90
+++ src/tests/test.f90
@@ -4267,6 +4267,7 @@ subroutine unit_test_linalg_fns(npass, nfail)
 			interpret_file(path//'test-06.syntran', quiet) == '[-1.000000E+00, -2.000000E+00, 3.000000E+00]', &
 			interpret_file(path//'test-07.syntran', quiet) == 'true', &
 			interpret_file(path//'test-08.syntran', quiet) == '2.000000000000000E+01', &
+			interpret_file(path//'test-09.syntran', quiet) == '0', &
 			.false.  & ! so I don't have to bother w/ trailing commas
 		]
 
